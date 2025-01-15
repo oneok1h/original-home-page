81ca79db52c2d9dadc454daf5ae8538d819892a2
oneok1h <buffalo_13h@icloud.com>
2025-01-15 20:18:21 +0900

    Add files via upload

---
+++ js/slidephoto.js
@@ -0,0 +1,14 @@
+﻿function slideSwitch() {
+	var $active = $('#slideshow li.active');
+	if ( $active.length == 0 ) $active = $('#slideshow li:last');
+	var $next =  $active.next().length ? $active.next() : $('#slideshow li:first');
+	$active.addClass('last-active');
+	$next.css({opacity: 0.0})
+		.addClass('active')
+		.animate({opacity: 1.0}, 2000, function() {
+			$active.removeClass('active last-active');
+		});
+}
+$(function() {
+	setInterval( "slideSwitch()", 8000 );
+});
